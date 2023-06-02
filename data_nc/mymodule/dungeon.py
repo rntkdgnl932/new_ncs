aa528b959f6bc9e1fc91defb40a892c04864e766
coob <koosanghwi@gmail.com>
2023-06-03 08:04:39 +0900

    ver 1.68

--- data_nc/mymodule/dungeon.py
+++ data_nc/mymodule/dungeon.py
@@ -402,6 +402,14 @@ def now_playing(cla, dun_):
                 print("던전 서브 완료", imgs_)
                 click_pos_reg(imgs_.x, imgs_.y, cla)
 
+            full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\dungeon\\different.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(520, 410, 620, 450, cla, img, 0.83)
+            if imgs_ is not None and imgs_ != False:
+                print("던전 중 실수로 다른 퀘스트 클릭한 경우", imgs_)
+                click_pos_2(410, 640, cla)
+
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\hunting_1.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
