ad0a01dae5bdaf48345567e2ddb23eec62d6389f
coob <koosanghwi@gmail.com>
2023-06-03 23:54:08 +0900

    ver 1.69

--- data_nc/mymodule/dungeon.py
+++ data_nc/mymodule/dungeon.py
@@ -973,6 +973,15 @@ def juljun_attack(cla, dun_):
                                                 imgs_ = imgs_set_(820, 80, 910, 120, cla, img, 0.83)
                                                 if imgs_ is not None and imgs_ != False:
                                                     click_pos_2(935, 100, cla)
+                                                    time.sleep(0.1)
+                                                full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\pvp_1.PNG"
+                                                img_array = np.fromfile(full_path, np.uint8)
+                                                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                                imgs_ = imgs_set_(900, 275, 960, 365, cla, img, 0.8)
+                                                if imgs_ is not None and imgs_ != False:
+                                                    print("menu_open 되어있음", imgs_)
+                                                    click_pos_2(930, 60, cla)
+                                                    time.sleep(0.1)
 
 
                                                 juljun_ready = True
