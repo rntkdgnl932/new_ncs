fe8c2ebcfa00ac70765eb7716be7774976fa0407
coob <koosanghwi@gmail.com>
2023-06-20 18:25:01 +0900

    ver 1.757

--- data_nc/mymodule/realtime.py
+++ data_nc/mymodule/realtime.py
@@ -80,7 +80,7 @@ def moogi_(cla):
                         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\moogi_sooglyun_2.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(450, 330, 560, 380, cla, img, 0.8)
+                        imgs_ = imgs_set_(450, 300, 560, 380, cla, img, 0.8)
                         if imgs_ is not None and imgs_ != False:
                             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\none_1.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
