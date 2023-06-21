4801d2142444f502fe7bf6b1b87ffd86e129c875
coob <koosanghwi@gmail.com>
2023-06-21 15:32:34 +0900

    ver 1.758

--- data_nc/mymodule/action.py
+++ data_nc/mymodule/action.py
@@ -1487,7 +1487,7 @@ def go_quest_ing_(cla):
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\grow\\grow_1\\quest_ing_1.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.83)
+        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("quest_ing_1", imgs_)
             go_ = True
@@ -1510,28 +1510,28 @@ def go_auto_ing_(cla):
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\grow\\grow_1\\auto_1.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.83)
+        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("auto_1", imgs_)
             go_ = True
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\grow\\grow_1\\auto_2.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.83)
+        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("auto_2", imgs_)
             go_ = True
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\grow\\grow_1\\auto_3.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.83)
+        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("auto_3", imgs_)
             go_ = True
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\grow\\grow_1\\auto_3.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.83)
+        imgs_ = imgs_set_(900, 820, 945, 860, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("auto_4", imgs_)
             go_ = True
