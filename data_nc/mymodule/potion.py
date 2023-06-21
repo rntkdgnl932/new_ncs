04f9caa0959f3c4c6cfbd5cc9a1423f7c7d19891
coob <koosanghwi@gmail.com>
2023-06-21 22:21:27 +0900

    ver 1.761

--- data_nc/mymodule/potion.py
+++ data_nc/mymodule/potion.py
@@ -330,7 +330,17 @@ def maul_potion(cla):
                     clean_screen(cla)
 
                     if jab_1_count > 5:
-                        jab_1_count = 0
+                        jab_1_count =
+
+                        full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\clean_screen\\exit_2.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(0, 0, 200, 600, cla, img, 0.83)
+                        if imgs_ is not None and imgs_ != False:
+                            print("maul potion exit_22", imgs_)
+                            click_pos_reg(imgs_.x, imgs_.y, cla)
+                            time.sleep(1)
+
                         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\maul_move_1.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
