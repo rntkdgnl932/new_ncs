0498a78e1adec7f9acef487e1ff5fccae16c8d18
coob <koosanghwi@gmail.com>
2023-06-30 00:24:20 +0900

    ver 1.81

--- data_nc/mymodule/test_.py
+++ data_nc/mymodule/test_.py
@@ -51,18 +51,18 @@ def go_test():
     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\clean_screen\\gabang_title.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(820, 80, 910, 120, cla, img, 0.8)
+    imgs_ = imgs_set_(820, 80, 910, 120, cla, img, 0.75)
     if imgs_ is not None and imgs_ != False:
-        print("clean screen 가방 닫자")
+        print("clean screen 가방 닫자", imgs_)
     else:
-        print("가방이 안보여>????")
+        print("가방이 안보여>????", imgs_)
 
-    full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\clean_screen\\gabang_title.PNG"
+    full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\clean_screen\\gabang_title2.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
     imgs_ = imgs_set_(820, 80, 910, 120, cla, img, 0.8)
     if imgs_ is not None and imgs_ != False:
-        print("clean screen 가방 닫자2")
+        print("clean screen 가방 닫자2", imgs_)
     else:
         print("가방이 안보여>????222", imgs_)
 
