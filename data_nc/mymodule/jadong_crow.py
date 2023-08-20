fffdd0be28dd889fa8c16bed68009ac41fc373bb
coob <koosanghwi@gmail.com>
2023-08-20 23:43:51 +0900

    ver 2.55

--- data_nc/mymodule/jadong_crow.py
+++ data_nc/mymodule/jadong_crow.py
@@ -837,12 +837,12 @@ def go_to_spot(cla, data):
                                 click_pos_reg(imgs_.x, imgs_.y, cla)
                                 print("플라잉 부스터!!!")
 
-                            full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\jadong\\flying_.PNG"
-                            img_array = np.fromfile(full_path, np.uint8)
-                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(820, 880, 910, 970, cla, img, 0.8)
-                            if imgs_ is not None and imgs_ != False:
-                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                            # full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\jadong\\flying_.PNG"
+                            # img_array = np.fromfile(full_path, np.uint8)
+                            # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            # imgs_ = imgs_set_(820, 880, 910, 970, cla, img, 0.8)
+                            # if imgs_ is not None and imgs_ != False:
+                            #     click_pos_reg(imgs_.x, imgs_.y, cla)
 
                         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\jadong\\fly_.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
