0cb3fdaa2ae9985a7d8e419cc3df6f7d68027e3a
coob <koosanghwi@gmail.com>
2023-09-11 00:31:50 +0900

    ver 3.04

--- data_nc/mymodule/gyucjunji.py
+++ data_nc/mymodule/gyucjunji.py
@@ -445,8 +445,17 @@ def now_playing(cla, lv):
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             time.sleep(0.2)
-                            mouse_move_cpp(480, 480, cla)
-                            time.sleep(0.1)
+
+                        else:
+                            full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\gyucjunji\\boss.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(600, 80, 910, 240, cla, img, 0.75)
+                            if imgs_ is not None and imgs_ != False:
+                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                                time.sleep(0.2)
+                        mouse_move_cpp(480, 480, cla)
+                        time.sleep(0.1)
 
                         # 포션 체크하기
                         potion_check(cla)
@@ -459,8 +468,16 @@ def now_playing(cla, lv):
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                             time.sleep(0.2)
-                            mouse_move_cpp(480, 480, cla)
-                            time.sleep(0.1)
+                        else:
+                            full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\gyucjunji\\boss.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(600, 80, 910, 240, cla, img, 0.75)
+                            if imgs_ is not None and imgs_ != False:
+                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                                time.sleep(0.2)
+                        mouse_move_cpp(480, 480, cla)
+                        time.sleep(0.1)
 
                         play_ = True
                         # 여긴 길드 지령 체크하기
