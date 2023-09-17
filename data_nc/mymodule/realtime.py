8cfe9c35e5e956f909efa1f905dc037451383779
coob <koosanghwi@gmail.com>
2023-09-17 10:34:16 +0900

    ver 3.3

--- data_nc/mymodule/realtime.py
+++ data_nc/mymodule/realtime.py
@@ -827,7 +827,17 @@ def jaelyo_(cla):
                                     time.sleep(0.5)
 
                             else:
-                                click_pos_2(20, 200, cla)
+
+                                for i in range(5):
+                                    full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\chango\\maul_chango_3.PNG"
+                                    img_array = np.fromfile(full_path, np.uint8)
+                                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                    imgs_ = imgs_set_(50, 80, 110, 110, cla, img, 0.8)
+                                    if imgs_ is not None and imgs_ != False:
+                                        break
+                                    else:
+                                        click_pos_2(20, 200, cla)
+                                    time.sleep(0.5)
 
                         in_chango_3 = False
                         in_chango_3_count = 0
