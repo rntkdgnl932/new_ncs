b791f5bde0b91a3dbcb8508fcb6b7462f0fe5da6
coob <koosanghwi@gmail.com>
2023-09-17 10:47:42 +0900

    ver 3.3

--- data_nc/mymodule/test_.py
+++ data_nc/mymodule/test_.py
@@ -74,7 +74,7 @@ def go_test():
         full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\chango\\" + jaelyo_ready[i] + ".PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(680, 90, 910, 880, cla, img, 0.8)
+        imgs_ = imgs_set_(680, 90, 910, 880, cla, img, 0.9)
         if imgs_ is not None and imgs_ != False:
             print("통합 : 재료 있", jaelyo_ready[i])
 
