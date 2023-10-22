549229d76b044b82aa1f4b9d9745d23cb9e835f3
coob <koosanghwi@gmail.com>
2023-10-22 09:03:05 +0900

    ver 4.25

--- data_nc/mymodule/test_.py
+++ data_nc/mymodule/test_.py
@@ -59,7 +59,7 @@ def go_test():
     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\auction\\2020.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(65, 995, 120, 1030, cla, img, 0.8)
+    imgs_ = imgs_set_(65, 995, 120, 1030, cla, img, 0.9)
     if imgs_ is not None and imgs_ != False:
         print("20/20", imgs_)
     else:
