d3c89e7c40f5f301fda8946eb293035bb912bdc3
coob <koosanghwi@gmail.com>
2023-12-20 22:53:55 +0900

    ver 4.72

--- data_nc/mymodule/action.py
+++ data_nc/mymodule/action.py
@@ -1591,14 +1591,14 @@ def clean_screen(cla):
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\dungeon\\gujum.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(450, 640, 490, 670, cla, img, 0.85)
+            imgs_ = imgs_set_(400, 880, 560, 960, cla, img, 0.9)
             if imgs_ is not None and imgs_ != False:
                 drag_pos(360, 550, 600, 550, cla)
 
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\dungeon\\juljun_ready.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(440, 660, 500, 710, cla, img, 0.85)
+            imgs_ = imgs_set_(400, 880, 560, 960, cla, img, 0.9)
             if imgs_ is not None and imgs_ != False:
                 drag_pos(360, 550, 600, 550, cla)
 
