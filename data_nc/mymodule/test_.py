b59da913e0ee33693d90c132c39f8ef762805d7e
coob <koosanghwi@gmail.com>
2024-03-09 15:56:02 +0900

    ver 4.961

--- data_nc/mymodule/test_.py
+++ data_nc/mymodule/test_.py
@@ -55,14 +55,14 @@ def go_test():
     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\maul_move_1.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(250, 960, 420, 1030, "three", img, 0.8)
+    imgs_ = imgs_set_(250, 960, 420, 1030, "three", img, 0.7)
     if imgs_ is not None and imgs_ != False:
         print("마을 이동서 있다.")
 
     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\random_move_1.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(250, 960, 420, 1030, "three", img, 0.8)
+    imgs_ = imgs_set_(250, 960, 420, 1030, "three", img, 0.7)
     if imgs_ is not None and imgs_ != False:
         print("랜덤 이동서 있다.")
 
