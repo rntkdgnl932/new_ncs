c8d4691fc6e51ce03d9842b89f47953bcd7efbf9
coob <koosanghwi@gmail.com>
2024-04-05 07:37:39 +0900

    ver 5.24

--- data_nc/mymodule/jadong_crow.py
+++ data_nc/mymodule/jadong_crow.py
@@ -1126,21 +1126,21 @@ def now_playing(cla):
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\hunting_1.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.8)
+            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.85)
             if imgs_ is not None and imgs_ != False:
                 print("자동 hunting_1", imgs_)
                 in_ = True
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\hunting_2.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.8)
+            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.85)
             if imgs_ is not None and imgs_ != False:
                 print("자동 hunting_2", imgs_)
                 in_ = True
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\check\\hunting_3.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.8)
+            imgs_ = imgs_set_(300, 850, 600, 900, cla, img, 0.85)
             if imgs_ is not None and imgs_ != False:
                 print("자동 hunting_3", imgs_)
                 in_ = True
