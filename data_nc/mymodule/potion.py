15ff19914801ff4a06e40641c41af400dfda1c9f
coob <koosanghwi@gmail.com>
2024-05-03 00:06:08 +0900

    ver 5.9321

--- data_nc/mymodule/potion.py
+++ data_nc/mymodule/potion.py
@@ -59,7 +59,7 @@ def potion_check(cla):
 
             v_.potion_count += 1
             print("물약 100개 이하로 파악된 횟수 => ", v_.potion_count)
-            if v_.potion_count > 2:
+            if v_.potion_count > 5:
                 v_.potion_count = 0
                 maul_potion_only(cla)
         else:
