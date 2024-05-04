90baef8f1bdb0d83a9e612fc48e70dd0ece7dadf
coob <koosanghwi@gmail.com>
2024-05-04 23:14:05 +0900

    ver 5.961

--- data_nc/mymodule/potion.py
+++ data_nc/mymodule/potion.py
@@ -103,7 +103,7 @@ def potion_check(cla):
             full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\dungeon\juljun_potion.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(250, 960, 750, 1030, cla, img, 0.7)
+            imgs_ = imgs_set_(100, 960, 750, 1030, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
                 what_potion_ = 'small'
                 x_reg = imgs_.x
@@ -112,7 +112,7 @@ def potion_check(cla):
                 full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\dungeon\juljun_potion_2.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(250, 960, 750, 1030, cla, img, 0.7)
+                imgs_ = imgs_set_(100, 960, 750, 1030, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
                     what_potion_ = 'middle'
                     x_reg = imgs_.x
