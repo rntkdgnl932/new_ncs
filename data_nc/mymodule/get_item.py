e51cad11b973e33b476f179a68c6171a6eb26ab8
coob <koosanghwi@gmail.com>
2024-07-11 18:25:08 +0900

    ver 6.01

--- data_nc/mymodule/get_item.py
+++ data_nc/mymodule/get_item.py
@@ -475,7 +475,7 @@ def get_event(cla):
                     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\get_item\\event_1.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(400, 300, 550, 350, cla, img, 0.8)
+                    imgs_ = imgs_set_(400, 300, 650, 360, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         get_season = True
 
@@ -595,7 +595,7 @@ def get_event(cla):
                                     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\get_item\\event_1.PNG"
                                     img_array = np.fromfile(full_path, np.uint8)
                                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                    imgs_ = imgs_set_(400, 300, 550, 350, cla, img, 0.8)
+                                    imgs_ = imgs_set_(400, 300, 650, 360, cla, img, 0.8)
                                     if imgs_ is not None and imgs_ != False:
                                         click_pos_2(870, 335, cla)
                                     else:
