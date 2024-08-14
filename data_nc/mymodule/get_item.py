b50f77035058ab0ab21399383d8a69c404efe341
coob <koosanghwi@gmail.com>
2024-08-14 23:46:03 +0900

    ver 6.04

--- data_nc/mymodule/get_item.py
+++ data_nc/mymodule/get_item.py
@@ -154,7 +154,15 @@ def get_sangjum_gyohwan(cla):
                         print("sohwan_moogi", imgs_)
                         break
                     else:
-                        click_pos_2(100, 200, cla)
+                        full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\get_item\\sohwan_moogi_title.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(170, 130, 370, 190, cla, img, 0.8)
+                        if imgs_ is not None and imgs_ != False:
+                            print("sohwan_moogi_title", imgs_)
+                            break
+                        else:
+                            click_pos_2(100, 200, cla)
                     time.sleep(0.5)
 
                 for i in range(10):
