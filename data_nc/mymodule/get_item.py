cc7ce3ac0c4d8445f19fbf5c7a0d50cb38efcab2
coob <koosanghwi@gmail.com>
2024-08-14 23:42:37 +0900

    ver 6.04

--- data_nc/mymodule/get_item.py
+++ data_nc/mymodule/get_item.py
@@ -138,7 +138,7 @@ def get_sangjum_gyohwan(cla):
                     full_path = "c:\\my_games\\nightcrow\\data_nc\\imgs\\get_item\\ilgwal_btn.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(0, 30, 120, 90, cla, img, 0.8)
+                    imgs_ = imgs_set_(30, 980, 140, 1040, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         break
                     else:
